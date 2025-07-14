7d1486946f1302a72dc25fab46d09c6f2f41cf50
Md Khalid Ansari <kansari1904@gmail.com>
2025-07-14 21:22:12 +0530

    Update

--- backend/mail/templates/courseEnrollmentEmail.js
+++ backend/mail/templates/courseEnrollmentEmail.js
@@ -68,7 +68,7 @@ exports.courseEnrollmentEmail = (courseName, name) => {
     <body>
         <div class="container">
             <a href=""><img class="logo" src="https://i.ibb.co/7Xyj3PC/logo.png"
-                    alt="StudyNotion Logo"></a>
+                    alt="CodeGyan Logo"></a>
             <div class="message">Course Registration Confirmation</div>
             <div class="body">
                 <p>Dear ${name},</p>
@@ -76,7 +76,8 @@ exports.courseEnrollmentEmail = (courseName, name) => {
                     are excited to have you as a participant!</p>
                 <p>Please log in to your learning dashboard to access the course materials and start your learning journey.
                 </p>
-                <a class="cta" href="https://study-notion-mern-stack.netlify.app/dashboard/enrolled-courses">Go to Dashboard</a>
+                
+                <a class="cta" href="https://code-gyan.vercel.app/dashboard/enrolled-courses">Go to Dashboard</a>
             </div>
             <div class="support">If you have any questions or need assistance, please feel free to reach out to us at 
             <a href="mailto:kansari1904@gmail.com">kansari1904@gmail.com</a>. We are here to help!</div>
